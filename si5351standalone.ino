ab861a72ce1ded69a7386a4d24099ab99f832c53
David Banks <dave@hoglet.com>
2021-06-10 12:56:56 +0100

    Use EEPROM to save settings

--- si5351standalone.ino
+++ si5351standalone.ino
@@ -5,6 +5,7 @@
 #include <LiquidCrystal.h>
 #include <EasyButton.h>
 #include <Encoder.h>
+#include <EEPROM.h>
 
 // Pin Assignments (for the Teensy 3.5 on the Breadboard)
 #define PIN_LCD_RS     2
@@ -31,13 +32,29 @@ EasyButton b2(PIN_BUTTON2);
 
 Encoder rotary(PIN_ROTARY_A, PIN_ROTARY_B);
 
-uint32_t freq[] = {
+#define NUM_CHANNELS 3
+
+#define MAGIC 0x31415927
+
+typedef struct {
+  uint32_t magic;
+  uint32_t count;
+  uint32_t freq[NUM_CHANNELS];
+  uint32_t step[NUM_CHANNELS];
+} t_settings;
+
+t_settings settings;
+
+uint32_t *freq = &settings.freq[0];
+uint32_t *step = &settings.step[0];
+
+uint32_t default_freq[NUM_CHANNELS] = {
    4000000,
    1843200,
   50000000
 };
 
-int step[] = {
+uint32_t default_step[NUM_CHANNELS] = {
     100000,
      10000,
     100000
@@ -58,8 +75,6 @@ unsigned int top_row = 0;
 
 #define NUM_ROWS 2
 
-#define NUM_CHANNELS (sizeof(freq) / sizeof(uint32_t))
-
 void print_frequency(unsigned int c) {
   uint32_t f = freq[c];
   if (f == 0) {
@@ -70,6 +85,29 @@ void print_frequency(unsigned int c) {
     lcd.print(fstr);
   }
 }
+
+void initialize_settings() {
+    settings.magic = MAGIC;
+    settings.count = 0;
+    for (unsigned int i = 0; i < NUM_CHANNELS; i++) {
+      settings.freq[i] = default_freq[i];
+      settings.step[i] = default_step[i];
+    }
+}
+
+void load_settings() {
+  EEPROM.get(0, settings);
+  if (settings.magic != MAGIC) {
+    initialize_settings();
+  }
+
+}
+
+void save_settings() {
+  settings.count++;
+  EEPROM.put(0, settings);
+}
+
 
 void update_display() {
   // Ensure the current channel is visible at all times
@@ -157,6 +195,7 @@ void setup(void)
   b1.onPressedFor(100, b1Pressed);
   b2.begin();
   b2.onPressedFor(100, b2Pressed);
+  load_settings();
   lcd.begin(16, 2);
   si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
   for (unsigned int c = 0; c < NUM_CHANNELS; c++) {
@@ -173,6 +212,17 @@ void loop(void) {
 
   b1.read();
   b2.read();
+
+  if (b1.isPressed() && b2.isPressed()) {
+    lcd.clear();
+    save_settings();
+    lcd.print("Saved Settings");
+    lcd.setCursor(0,1);
+    lcd.print(settings.count);
+    lcd.print(" EE Writes");
+    delay(2000);
+    update_display();
+  }
 
   r = (rotary.read() / 4) * step[channel];
   if (r < 0) {
